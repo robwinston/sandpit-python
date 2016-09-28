42389a01f822d49805fbbe9920f7cd5627ca2a92
Rob Winston <robtwinston@gmail.com>
2016-09-28 06:03:31 +0100

    Refactor wip 5

--- utils/utilcode.py
+++ utils/utilcode.py
@@ -57,6 +57,36 @@ def get_existing_package_name(lines):
 reserved_words = ['while', 'final', 'static', 'switch']
 
 
+def full_package_dir():
+    pass
+
+
+def add_original_package_names(file_content_pairs):
+    return [(file_name, get_existing_package_name(its_lines), its_lines)
+            for (file_name, its_lines) in file_content_pairs]
+
+
+def unique_package_names(file_package_pairs):
+    return set([package_name for (_, package_name, _) in file_package_pairs]) - {None}
+
+
+def common_package_root(package_names):
+    if len(package_names) == 0:
+        return None
+    if len(package_names) == 1:
+        return list(package_names)[0]
+
+    package_nodes = [package_name.split('.') for package_name in package_names]
+    shortest_len = min([len(nodes) for nodes in package_nodes])
+    root_nodes = []
+    for idx in range(0, shortest_len):
+        if len(set([nodes[idx] for nodes in package_nodes])) == 1:
+            root_nodes.append(package_nodes[0][idx])
+        else:
+            break
+    return '.'.join(root_nodes) if len(root_nodes) > 0 else None
+
+
 def cleanse_package_name(pkg_name):
     if pkg_name is None:
         return None
@@ -78,64 +108,49 @@ def cleanse_package_name(pkg_name):
         return '.'.join(remove_all_but_last_if_duplicated(pkg_name.split('.')))
 
 
-def add_or_modify_package_name(lines):
-    return '', lines
+def add_or_modify_package_name(file_with_pkg_and_content, common_pkg_root):
+    src_file_name, original_package_name, original_file_contents = file_with_pkg_and_content
+
+    pkg_suffix = None if original_package_name is None or len(original_package_name) == len(common_pkg_root) \
+        else original_package_name[len(common_pkg_root.split('.')):]
+
+    # TODO this approach seems ugly ...
+    src_path_bits = src_file_name.split('/')
+    prj_node = src_path_bits[-2]
+    lecture_node = src_path_bits[-3]
+
+    new_package_name = cleanse_package_name('.'.join([common_pkg_root, lecture_node, prj_node]))
+    if pkg_suffix is not None:
+        '.'.join([new_package_name, pkg_suffix])
+
+    new_package_line = str.format('package {};', new_package_name)
+
+    if original_package_name is None:
+        new_file_contents = [new_package_line] + original_file_contents
+    else:
+        new_file_contents = [new_package_line] + original_file_contents[1:]
+
+    return new_package_name, new_file_contents
 
 
 def modify_imports(lines):
     return lines
 
-def full_package_dir():
-    pass
-
-
-def all_package_names(file_content_pairs):
-    return [(file_name, get_existing_package_name(its_lines)) for (file_name, its_lines) in file_content_pairs]
-
-
-def unique_package_names(file_package_pairs):
-    return set([package_name for (_, package_name) in file_package_pairs]) - {None}
-
-
-def common_package_root(package_names):
-    if len(package_names) == 0:
-        return None
-    if len(package_names) == 1:
-        return package_names[0]
-    
-    package_nodes = [package_name.split('.') for package_name in package_names]
-    shortest_len = min([len(nodes) for nodes in package_nodes])
-    root_nodes = []
-    for idx in range(0, shortest_len):
-        if len(set([nodes[idx] for nodes in package_nodes])) == 1:
-            root_nodes.append(package_nodes[0][idx])
-        else:
-            break
-    return '.'.join(root_nodes) if len(root_nodes) > 0 else None
-
-
-
-def process_java_lines(its_lines):
-
-    pkg_name, its_lines = add_or_modify_package_name(its_lines)
-    its_lines = modify_imports(its_lines)
-    its_lines = add_or_modify_doc_string(its_lines)
-
-    return pkg_name, its_lines
-
 
 # java files need their package names altered to match their destination
 def process_java_files(files_to_process):
     # create a list of pairs ("pathless" file name, its content as a list of lines)
-    file_content_pairs = [(os.path.basename(file_to_process), all_lines(file_to_process))
-                          for file_to_process in files_to_process]
-    all_pkg_names = all_package_names(file_content_pairs)
-    unique_pkg_names = unique_package_names(all_pkg_names)
-    print('Files: {}, pkg names: {}, unq pns: {}'.format(len(file_content_pairs), len(all_pkg_names), len(unique_pkg_names)))
-    if len(unique_pkg_names) > 1:
-        print(unique_pkg_names)
-        cpr = common_package_root(unique_pkg_names)
-        print("common root: " + cpr)
+    files_with_content = [(file_to_process, all_lines(file_to_process)) for file_to_process in files_to_process]
+    files_with_pkg_and_content = add_original_package_names(files_with_content)
+    unique_pkg_names = unique_package_names(files_with_pkg_and_content)
+    common_pkg_root = common_package_root(unique_pkg_names)
+    for file_with_pkg_and_content in files_with_pkg_and_content:
+        new_pkg_name, new_file_contents = add_or_modify_package_name(file_with_pkg_and_content, common_pkg_root)
+        new_file_contents = modify_imports(new_file_contents)
+        new_file_contents = add_or_modify_doc_string(new_file_contents)
+        print('here')
+
+
 
 
 
